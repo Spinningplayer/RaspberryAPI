053abcc6e5832095f992230ca94de8680d07605f
thijs <Thijsvanmarle@gmail.com>
2017-12-13 16:31:51 +0100

    password update...

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -1,7 +1,7 @@
 var neo4j = require('neo4j-driver').v1;
 var Server = require('../models/server');
 
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic('neo4j', 'neo4j'));
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic('neo4j', 'Welkom123'));
 var session = driver.session();
 
 module.exports = {
