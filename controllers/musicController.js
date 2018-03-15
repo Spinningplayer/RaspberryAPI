81a04328aa07891a8a44e98ebd5adb69d22390d1
thijs <Thijsvanmarle@gmail.com>
2018-03-15 21:59:25 +0100

    Added Post method for songs
    Added a post method for songs in the musicController

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,4 +1,7 @@
+var Song = require('../models/song')
 
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic('neo4j', 'Welkom123'));
+var session = driver.session();
 
 module.exports = {
     postPlaylist(req, res) {
@@ -22,6 +25,41 @@ module.exports = {
     },
 
     postSong(req, res) {
+      var body = req.body;
+      session.run(
+        'CREATE (n:Song {title: {titleParam}, artist: {artistParam}, url: {urlParam}}) RETURN n',
+        {
+          titleParam: body.title,
+          artistParam: body.artist,
+          urlParam: body.url
+        })
+        .then(result => {
+          var id = result.records[0]._fields.identity.low;
+
+          session.run(
+            'MERGE (n:Genre {genre: {genreParam}} return n',
+            {
+              genreParam: body.genre
+            })
+            .then(Genre => {
+              session.run(
+                'MATCH (s:Song), (g:Genre) WHERE id(s)=toInt({songParam}) AND id(g)=toInt({genreParam}) ' +
+                'CREATE (s)-[r:IS_OF_GENRE]->(g)',
+                {
+                  songParam: id,
+                  genreParam: Genre.records[0]._fields.identity.low
+                })
+                .then(x => {
+                  res.status(200)
+                  res.json({"msg":"Song Created"})
+              })
+            })
+        })
+        .catch(err => {
+          console.log(err);
+
+          res.status(500).json(err);
+        })
 
     },
 
